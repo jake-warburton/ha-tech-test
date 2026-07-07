14c2a0f63cada5623fcba544cbcbb1ad346577e1
jake-warburton <jakewarburton6@gmail.com>
2026-07-07 22:58:23 +0100

    Display resource card summary

--- src/components/molecules/ResourceCard/ResourceCard.tsx
+++ src/components/molecules/ResourceCard/ResourceCard.tsx
@@ -5,9 +5,18 @@ type ResourceCardProps = {
 };
 
 export const ResourceCard = ({ resource }: ResourceCardProps) => {
+  const visibleTags = resource.tags.slice(0, 3);
+
   return (
     <article>
+      <img src={resource.thumbnail} alt={`${resource.title} thumbnail`} />
       <h3>{resource.title}</h3>
+      <ul>
+        {visibleTags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+      <p>{resource.duration} min</p>
     </article>
   );
 };
