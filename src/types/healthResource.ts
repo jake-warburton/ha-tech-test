047782a58352d17789b961a26160d5a8e8770c48
jake-warburton <jakewarburton6@gmail.com>
2026-07-07 15:24:14 +0100

    Add healthResource data and type

---
+++ src/types/healthResource.ts
@@ -0,0 +1,10 @@
+export type HealthResource = {
+  id: string;
+  category: string;
+  title: string;
+  thumbnail: string;
+  tags: string[];
+  duration: number;
+  description: string;
+  date_uploaded: string;
+};
